7c8fe5b63334c600498d3fdf7c4371d820f5ed51
Emlyn Bolton-Maggs <eb@expahotels.com>
2018-09-16 12:55:47 -0700

    Fix up TS defs

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,14 @@
 import { Storage } from "redux-persist";
+import { RNSensitiveInfoOptions } from "react-native-sensitive-info";
+
 export interface ReduxPersistSensitiveStorage extends Storage {
-  /**
-   * Fetches key and passes the result to callback, along with an Error if there is any.
-   */
-  getItem(
-    key: string,
-    callback?: (error?: Error, result?: string) => void
-  ): Promise<string | null>;
-
-  /**
-   * Sets value for key and calls callback on completion, along with an Error if there is any
-   */
-  setItem(
-    key: string,
-    value: string,
-    callback?: (error?: Error) => void
-  ): Promise<void>;
-
   getAllKeys(
     callback?: (error?: Error, result?: [string]) => void
   ): Promise<[string] | null>;
 }
+
+declare function createSensitiveStorage(
+  options: RNSensitiveInfoOptions
+): ReduxPersistSensitiveStorage
+
+export default createSensitiveStorage;
